082f0c6db0eb406080dc0eea05ee24ba997900e2
pratheeshraj <rajeshnsk5@gmail.com>
2023-03-01 10:10:12 +0530

    a

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const server = require('http').createServer(app);
 const PORT = 5001;
 const io = require('socket.io')(server, {
   cors: {
-    origin: 'https://master--soft-elf-4de1fb.netlify.app',
+    origin: '*',
     methods: ['GET', 'POST']
   }
 })
