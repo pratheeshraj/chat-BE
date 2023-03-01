f5772c5d3c97463c851a69169156b2cd07c11277
pratheeshraj <110892110+pratheeshraj@users.noreply.github.com>
2023-03-01 09:58:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ const cors = require('cors');
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    orgin: "*",
+}));
 
 app.use('/users', userRoutes)
 require('./connection')
@@ -16,9 +18,7 @@ require('./connection')
 const server = require('http').createServer(app);
 const PORT = 5001;
 const io = require('socket.io')(server, {
-cors({
-    orgin: "*",
-});
+
 
 
 async function getLastMessagesFromRoom(room){
